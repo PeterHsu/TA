94c3c6ffb7b31e55bc2a88ee9f9ea15f961679bb
Peter <peter.hsu@concords.com.tw>
2015-10-13 14:09:12 +0800

    step3
    完成CRUD

--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -7,4 +7,28 @@ router.get('/api',function(req, res, next){
     res.json(data);
   });
 });
+router.post('/api',function(req,res,next){
+  stock.create(req.body,function(err,data){
+    if(err) return next(err);
+    res.json(data);
+  });
+});
+router.get('/api/:id',function(req,res,next){
+  stock.findById(req.params.id,function(err,data){
+    if(err) return next(err);
+    res.json(data);
+  });
+});
+router.put('/api/:id',function(req,res,next){
+  stock.findByIdAndUpdate(req.params.id,req.body,function(err,data){
+    if(err) return next(err);
+    res.json(data);
+  });
+});
+router.delete('/api/:id',function(req,res,next){
+  stock.findByIdAndRemove(req.params.id,function(err,data){
+    if(err) return next(err);
+    res.json(data);
+  });
+});
 module.exports = router;
