e115f7bda887d927aca4086d7398f1aa2ec8b6cb
Peter Hsu <petahsu@gmail.com>
2015-10-14 23:13:19 +0800

    step7
    完成CRUD

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 angular.module('TA',['ngRoute','ngResource'])
 .factory('Stock',function($resource){
-  return $resource('/stock/api/:id',{},{
+  return $resource('/stock/api/:id',{id:'@_id'},{
     update:{method:'PUT'}
   });
 })
@@ -12,4 +12,33 @@ angular.module('TA',['ngRoute','ngResource'])
         $scope.stocks = Stock.query();
       }
   };
+})
+.config(function($routeProvider){
+  $routeProvider
+  .when('/',{
+    redirectTo:'/index'
+  })
+  .when('/index',{
+    templateUrl:'/pages/stock/index.html',
+    controller:function($scope, Stock){
+      $scope.stocks = Stock.query();
+
+      $scope.stock = new Stock();
+      $scope.save = function(stock){
+        stock.$save(stock);
+        $scope.stocks = Stock.query();
+      }
+      $scope.get = function(stock){
+        $scope.stock = Stock.get({id:stock._id});
+      }
+      $scope.delete = function(stock){
+        stock.$delete({id:stock._id});
+        $scope.stocks = Stock.query();
+      }
+      $scope.update = function(stock){
+        Stock.update(stock);
+        $scope.stocks = Stock.query();
+      }
+    }
+  })
 });
